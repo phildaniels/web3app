3eb0fc2f021b34dec4a7d652bbfcc70a7c4e5582
Phil Daniels <05879@global.ul.com>
2022-06-20 15:17:33 -0600

    adding hardhat

---
+++ test/index.ts
@@ -0,0 +1,27 @@
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+
+describe('ULMARK', function () {
+  it('Should mint and transfer the ULMARK to someone', async function () {
+    const ULMark = await ethers.getContractFactory('ULMark');
+    const ulMark = await ULMark.deploy();
+    await ulMark.deployed();
+
+    const recepient = '0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199';
+    const metaDataUri = 'cid/ulmark.pdf';
+
+    let balance = await ulMark.balanceOf(recepient);
+    expect(balance).to.equal(0);
+
+    const newlyMintedULMark = await ulMark.payToMint(recepient, metaDataUri, {
+      value: ethers.utils.parseEther('0.05'),
+    });
+
+    await newlyMintedULMark.wait();
+
+    balance = await ulMark.balanceOf(recepient);
+    expect(balance).to.equal(1);
+
+    expect(await ulMark.isContentOwned(metaDataUri)).to.equal(false);
+  });
+});
