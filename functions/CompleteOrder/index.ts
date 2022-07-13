838b0f344023ad189defc6b0166801526bb9334c
Phil Daniels <05879@global.ul.com>
2022-07-13 17:33:16 -0600

    more changes

--- functions/CompleteOrder/index.ts
+++ functions/CompleteOrder/index.ts
@@ -11,12 +11,19 @@ import {
   Order,
   OrderModel,
 } from '../data/order';
+import axios from 'axios';
+import { EventGridEvent } from '../data/event-grid-event.model';
+import { OrderCompletedEvent } from '../data/order-completed-event.model';
+import uuid from 'uuid';
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
   try {
+    if (process.env['CurrentEnvironment'] !== 'Production') {
+      (process.env['NODE_TLS_REJECT_UNAUTHORIZED'] as any) = 0;
+    }
     if (!completeOrderSchemaValidator(req.body)) {
       context.res = badRequestResult(
         'Invalid order completion',
@@ -35,6 +42,32 @@ const httpTrigger: AzureFunction = async function (
       { _id: orderId },
       updateObject
     );
+    const eventGridEvent = {
+      topic:
+        '/subscriptions/5b4b650e-28b9-4790-b3ab-ddbd88d727c4/resourcegroups/test/providers/Microsoft.EventHub/namespaces/test',
+      subject: 'order',
+      eventType: 'completed',
+      eventTime: new Date(),
+      id: uuid.v4(),
+      data: {
+        orderId,
+      },
+      dataVersion: '',
+      metadataVersion: '1',
+    } as EventGridEvent<OrderCompletedEvent>;
+    const response = await axios.post(
+      process.env['EventGridEndpoint'] ?? '',
+      eventGridEvent
+    );
+    if (response.status !== 202) {
+      await mongooseClient.updateAsync<Order>(
+        OrderModel,
+        { _id: orderId },
+        { orderCompleted: false, orderPrice: null }
+      );
+      throw 'EventGrid failed to send event';
+    }
+
     context.res = okResult('Order updated');
   } catch (e) {
     context.log.error('Orders function failed with error', e);
